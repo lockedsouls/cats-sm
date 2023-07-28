ba21f5378fae6ccc9bf8d4b29c022978269654bf
lockedsouls <lockedssouls@gmail.com>
2023-07-28 22:23:47 +0300

    Setup frontend routes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./styles/index.css";
 
+const Routes = createBrowserRouter([
+    {
+        path: "*",
+        element: <h1>Not found</h1>
+    },
+    {
+        path: "/",
+        element: <h1>Content here</h1>
+    }
+]);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>Balls</h1>);
+root.render(<RouterProvider router={Routes}/>);
