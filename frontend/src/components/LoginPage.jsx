ca872de821206f53f485609870c1710e68b27908
Popa-Calin <popacalin2510@gmail.com>
2023-07-30 15:13:02 +0300

    notfinished, creating the design

--- frontend/src/components/LoginPage.jsx
+++ frontend/src/components/LoginPage.jsx
@@ -4,12 +4,12 @@ export default function LoginPage(){
     
     return (
         <div className="LoginPage">
-          <h1>Welcome to Cat Posts</h1>
+          <h1>Welcome</h1>
           <form>
             <input type="text" placeholder="Username/Email" />
             <input type="password" placeholder="Password" />
     
-            <button type="submit">Log In</button>
+            <button type="submit">Log in</button>
           </form>
         </div>
       );
